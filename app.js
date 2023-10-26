a23018a4099b35026627a7729b1b4ec25d5ff6d2
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2023-10-26 20:05:28 -0300

    Estructura inicial

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = 3030;
 const path = require('path');
 
-
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
